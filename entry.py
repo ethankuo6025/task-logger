3d0dbd3e48271981592b09d0ef1b20e4772b1c3e
Ethan Kuo <ethankuocodes@gmail.com>
2025-10-10 01:11:57 -0400

    reupdate

--- entry.py
+++ entry.py
@@ -1,56 +1,44 @@
 from datetime import date, time, timedelta
 from sqlalchemy import Interval, MetaData, Table, text, insert
 from engine import create_engine
+from enum import Enum, auto
 
-# sql shortcut to log new entry
-def log(name:str, date:date, 
-        start_time: time, duration:timedelta, 
-        description:str = '', sub_tasks:int = 1):
-    
-    engine = create_engine()
-    metadata = MetaData()
+class EntryParameterType(Enum):
+    TITLE = auto()
+    NOTES = auto()
+    START_TIME = auto()
+    DURATION = auto()
 
-    table = Table("logs", metadata, autoload_with=engine)
+class EntryParameter():
+    def __init__(self, parameter_type: EntryParameterType, parameter_value: str):
+        self.parameter_type = parameter_type
+        self.parameter_value = parameter_value
     
-    stmt = insert(table).values(name=name, date=date,
-                                start_time=start_time, duration=duration,
-                                description=description, sub_tasks=sub_tasks)
+    '''
+    validation based on the parameter_type
+    '''
+    def validate():
+        return
     
-    with engine.connect() as conn:
-        conn.execute(stmt)
-        conn.commit()
+'''
+sql shortcut for logging a new entry and linking tags
+input: all the necessary fields
+output: should be true unless something goes horribly wrong. 
+    data should be cleaned before it ever gets here. 
+'''
+def log_entry():
     return
 
-# function for grabbing a single field
-def grab_input(field_name):
-    while True:
-        val = input("\nEnter the activity's {field_name}: ")
-        while True:
-            check = input(f'Is "{val}" correct? y/n: ').lower()
-            if check in ("y", "n"):
-                break
-            print("Invalid input. Try again.")
-        
-        if check == "y":
-            break
-    return val
+'''
+normalize tags into standardized format. stripped, lowercased, spaces instead of hyphens or underscores or anything.
+'''
+def normalize_tags():
+    return
 
+
+'''
+main()
+'''
 if __name__ == '__main__':
-    # entry()
-    # log("Scrolling", date(2025, 7, 30), time(hour=7, minute=0), duration=timedelta(hours=2))
-    engine = create_engine()
-    from engine import connect
-    df = connect(engine)
-    print(df.head())
+    print("hello world")
     
-'''
-CREATE TABLE logs (
-    id                  SERIAL PRIMARY KEY,
-    name                TEXT NOT NULL,
-    date                DATE NOT NULL,
-    start_time          TIME NOT NULL,
-    duration            INTERVAL NOT NULL,
-    description         TEXT,
-    sub_tasks           INTEGER DEFAULT 1 CHECK (sub_tasks >= 1)
-);
-'''
